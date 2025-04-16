0fe1067938bfbc5f9be32d14ba75f9b366384d1b
tr0ncoduro <erick.parada101@gmail.com>
2025-04-16 14:38:58 -0600

    its a 3 git add .

--- runWizard.bat
+++ runWizard.bat
@@ -34,9 +34,42 @@ if %errorlevel% neq 0 (
 :: Change to the directory where the batch file is located
 cd /d "%~dp0"
 
-:: Run cargo run in the current directory
-echo Running cargo run in directory: %cd%...
-cargo run
+:: Build the Rust application in release mode
+echo Building the application in release mode...
+cargo build --release
+if %errorlevel% neq 0 (
+    echo Failed to build the application in release mode.
+    pause
+    exit /b %errorlevel%
+)
+
+:: Get the path to the release binary
+set "RELEASE_BINARY=%~dp0target\release\your_app_name.exe"
+if not exist "%RELEASE_BINARY%" (
+    echo Release binary not found at: %RELEASE_BINARY%
+    pause
+    exit /b 1
+)
+
+:: Create a desktop shortcut using PowerShell
+echo Creating a desktop shortcut for the application...
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+    "$ShortcutPath = [System.IO.Path]::Combine([Environment]::GetFolderPath('Desktop'), 'YourAppName.lnk'); " ^
+    "$WScriptShell = New-Object -ComObject WScript.Shell; " ^
+    "$Shortcut = $WScriptShell.CreateShortcut($ShortcutPath); " ^
+    "$Shortcut.TargetPath = '%RELEASE_BINARY%'; " ^
+    "$Shortcut.WorkingDirectory = '%~dp0'; " ^
+    "$Shortcut.Save();"
+
+if %errorlevel% neq 0 (
+    echo Failed to create the desktop shortcut.
+    pause
+    exit /b %errorlevel%
+)
+
+:: Run the release binary
+echo Running the release version of the application...
+start "" "%RELEASE_BINARY%"
 
 :: Pause to allow the user to see the output
 pause
